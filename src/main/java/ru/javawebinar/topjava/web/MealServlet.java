7fe81c20e848e3f00fe196ebd5f1d48dc129a6d1
blaec <odeskonst@yandex.ru>
2017-11-08 22:54:01 +0200

    Draft setAttribute

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,18 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -17,7 +23,12 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        // Get filtered meals list with exceed parameter
+        List<Meal> meals = MealsUtil.getMeals();
+        List<MealWithExceed> mealsWithExceeded = MealsUtil.getFilteredWithExceeded(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+
+        request.setAttribute("mealsList", mealsWithExceeded);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+//        response.sendRedirect("meals.jsp");
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,9 +1,17 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
 <html>
 <head>
     <title>Meals</title>
 </head>
 <body>
-    Meals text goes here...
+    <h3><a href="index.html">Home</a></h3>
+
+    <c:forEach items="${mealsList}" var="meal">
+    <h2>
+        <c:out value="${meal}"/>
+    </h2>
+    </c:forEach>
 </body>
 </html>
