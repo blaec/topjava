10187c851074d28747c12967e33328000ea03231
blaec <odeskonst@yandex.ru>
2017-11-23 07:14:57 +0200

    attempts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,7 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories) VALUES
+  ('2015-05-30 10:00', 'Завтрак', 500),
+  ('2015-05-30 13:00', 'Обед', 1000);
