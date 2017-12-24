d1b511c1ba98413936d9891d6ad9a4da019e57c0
blaec <odeskonst@yandex.ru>
2017-12-25 00:12:15 +0200

    h07010-1 added_controller_tests [probably not full]

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.web;
+
+import com.sun.xml.internal.ws.api.pipe.ContentType;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testCss() throws Exception{
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+//                .andExpect(content().contentType(MediaType.valueOf("text/css;pageEncoding=UTF-8")))
+//                .andExpect(content().contentType("text/css;charset=ISO-8859-1"))
+                .andExpect(content().contentTypeCompatibleWith("text/css"));
+//                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN));
+    }
+}
